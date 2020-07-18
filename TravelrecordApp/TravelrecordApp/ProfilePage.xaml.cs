fccd5332573c9eda10731f767694cd6a0d15085f
Daniel <daniel.andersson@iths.se>
2020-07-18 13:06:48 +0200

    80 done, section 10 done

--- TravelrecordApp/TravelrecordApp/ProfilePage.xaml.cs
+++ TravelrecordApp/TravelrecordApp/ProfilePage.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using TravelrecordApp.Model;
@@ -24,6 +25,22 @@ namespace TravelrecordApp
             using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
             {
                 var postTable = conn.Table<Post>().ToList();
+
+                var categories = (from p in postTable
+                                  orderby p.CategoryId
+                                  select p.CategoryName).Distinct().ToList();
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+
+                foreach (var category in categories)
+                {
+
+                    var count = postTable.Where(p => p.CategoryName == category).ToList().Count();
+
+                    categoriesCount.Add(category, count);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
 
                 postCountLabel.Text = postTable.Count.ToString();
             }
